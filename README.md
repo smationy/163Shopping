d21fc0973123a322594a58b0d63064a78a46be3c
cyww <changyanwuwei@outlook.com>
2019-10-09 07:34:13 +0800

    甄选家完成

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 网易严选
+# copy网易严选
 
+## css
+
+button ===> div中套span。
+
+文本垂直居中可以使用display：flex来做
+
+## 进度
+
+甄选家	完成
--- src/pages/Knowledge/Select/Select.vue
+++ src/pages/Knowledge/Select/Select.vue
@@ -8,8 +8,9 @@
             <div class="swiper-slide"><img src="https://yanxuan.nosdn.127.net/0ab7c3d8e18630076aaf32e2e70ae41e.jpg?imageView&quality=75&thumbnail=670x371" alt=""></div>
         </div>
       </div>
-      <div class="bg"></div>
+      
       <div class="collection">
+        <div class="bg"></div>
         <h2>建议征集 <i class="iconfont icon-jiantou2"></i></h2>
         <p class="standard">告诉我们，你的好物标准</p>
         <div class="board">
@@ -31,6 +32,183 @@
         <p class="text">每一个厨房都离不开一块好菜板，卫生、环保的同时，又要兼顾美感。你会希望严选新推出的菜板是使用什么材质都呢？造型是圆形还是长方形？快来和选妹谈谈你的想法吧！</p>
         <div class="btn"><span>我要建议</span></div>
       </div>
+      <div class="selection">
+        <div class="bg"></div>
+        <div class="collection selHeader">
+          <h2>样式甄选<i class="iconfont icon-jiantou2"></i> </h2>
+          <p class="standard">告诉我们，你的好物标准</p>
+        </div>
+        <div class="center">
+          <div class="header">
+            <div >
+              <span class="special">
+                <span>居家</span>
+                <span>篇</span>
+              </span>
+              <span class="title">不如睡觉系列床品</span>
+            </div>
+            <p>提升睡眠幸福感，甄选你喜欢的款式</p>
+          </div>
+          <ul class="shops">
+            <li>
+              <img src="https://yanxuan.nosdn.127.net/46d98c36c3c989dc3fa386d052fb0b2d.png?imageView&quality=65&thumbnail=300x300" alt="">
+              <p><span>健身不如睡觉</span><span>深灰色</span></p>
+              <div class="btn"><i class="iconfont icon-icon-test ">喜欢</i></div>
+            </li>
+            <li>
+              <img src="https://yanxuan.nosdn.127.net/46d98c36c3c989dc3fa386d052fb0b2d.png?imageView&quality=65&thumbnail=300x300" alt="">
+              <p><span>健身不如睡觉</span><span>深灰色</span></p>
+              <div class="btn"><i class="iconfont icon-icon-test ">喜欢</i></div>
+            </li>
+            <li>
+              <img src="https://yanxuan.nosdn.127.net/46d98c36c3c989dc3fa386d052fb0b2d.png?imageView&quality=65&thumbnail=300x300" alt="">
+              <p><span>健身不如睡觉</span><span>深灰色</span></p>
+              <div class="btn"><i class="iconfont icon-icon-test ">喜欢</i></div>
+            </li>
+            <li>
+              <img src="https://yanxuan.nosdn.127.net/46d98c36c3c989dc3fa386d052fb0b2d.png?imageView&quality=65&thumbnail=300x300" alt="">
+              <p><span>健身不如睡觉</span><span>深灰色</span></p>
+              <div class="btn"><i class="iconfont icon-icon-test ">喜欢</i></div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="tryOut">
+        <div class="bg"></div>
+        <div class="collection selHeader">
+          <h2>样品试用<i class="iconfont icon-jiantou2"></i> </h2>
+          <p class="standard">加入我们，一起检验好物初样</p>
+        </div>
+        <div class="content">
+          <div class="detail">
+            <div class="pic">
+              <img src="https://yanxuan.nosdn.127.net/6f6bb83f0b6daf0ef6108b3caf0a01e3.jpg?imageView&quality=75&thumbnail=750y550" alt="">
+              <div class="time">
+                <span>距报名结束还有：</span>
+                <span>02天04时47分04秒</span>
+              </div>
+            </div>
+            <h2>大英博物馆香薰蜡烛</h2>
+            <p class="status"><span>状态：</span><span>申请中</span></p>
+            <div class="card">
+              <div >
+                <p class="red">59元</p>
+                <p>参考价</p>
+              </div>
+              <div>
+                <p class="black">1人</p>
+                <p>招募甄选家</p>
+              </div>
+            </div>
+            <div class="btn">
+              <span>立即申请</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="result">
+        <div class="bg"></div>
+        <div class="collection selHeader">
+          <h2>甄选结果<i class="iconfont icon-jiantou2"></i> </h2>
+          <p class="standard">因为你们，这些好物得以面世</p>
+        </div>
+        <ul class="shops">
+          <li class="shop">
+            <a >
+              <div class="pic">
+                <img src="https://yanxuan-item.nosdn.127.net/a5136b0edc99dd0db5bafdfc688125a5.png?imageView&quality=65&thumbnail=330x330" alt="">
+                <ul class="chose">
+                  <li>2</li>
+                  <li>色</li>
+                  <li>可</li>
+                  <li>选</li>
+                </ul>
+              </div>
+              <div class="text">
+                <p class="offer">抄底特惠</p>
+                <p class="title">男式经典羊毛羊绒大衣</p>
+                <p class="pir">¥1391</p>
+              </div>
+            </a>
+          </li>
+          <li class="shop">
+            <a >
+              <div class="pic">
+                <img src="https://yanxuan-item.nosdn.127.net/a5136b0edc99dd0db5bafdfc688125a5.png?imageView&quality=65&thumbnail=330x330" alt="">
+                <ul class="chose">
+                  <li>2</li>
+                  <li>色</li>
+                  <li>可</li>
+                  <li>选</li>
+                </ul>
+              </div>
+              <div class="text">
+                <p class="offer">抄底特惠</p>
+                <p class="title">男式经典羊毛羊绒大衣</p>
+                <p class="pir">¥1391</p>
+              </div>
+            </a>
+          </li>
+          <li class="shop">
+            <a >
+              <div class="pic">
+                <img src="https://yanxuan-item.nosdn.127.net/a5136b0edc99dd0db5bafdfc688125a5.png?imageView&quality=65&thumbnail=330x330" alt="">
+                <ul class="chose">
+                  <li>2</li>
+                  <li>色</li>
+                  <li>可</li>
+                  <li>选</li>
+                </ul>
+              </div>
+              <div class="text">
+                <p class="offer">抄底特惠</p>
+                <p class="title">男式经典羊毛羊绒大衣</p>
+                <p class="pir">¥1391</p>
+              </div>
+            </a>
+          </li>
+          <li class="shop">
+            <a >
+              <div class="pic">
+                <img src="https://yanxuan-item.nosdn.127.net/a5136b0edc99dd0db5bafdfc688125a5.png?imageView&quality=65&thumbnail=330x330" alt="">
+                <ul class="chose">
+                  <li>2</li>
+                  <li>色</li>
+                  <li>可</li>
+                  <li>选</li>
+                </ul>
+              </div>
+              <div class="text">
+                <p class="offer">抄底特惠</p>
+                <p class="title">男式经典羊毛羊绒大衣</p>
+                <p class="pir">¥1391</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="monReport">
+        <div class="bg"></div>
+        <h2>甄选家月报</h2>
+        <div class="pic" ref="pic">
+          <ul class="reports">
+            <li class="report">
+              <img src="https://yanxuan.nosdn.127.net/c365674c1a28e094e8b74339f3024749.jpeg?imageView&quality=75&thumbnail=550y356" alt="">
+              <div class="text">
+                <h3>网易严选3周年-粉丝生日趴</h3>
+                <p>3月末，我们分别在广州、北京、杭州的有家民宿严选HOME，还有长沙严选粉丝-来来的家 一起提前庆祝严选生日！</p>
+              </div>
+            </li>
+            <li class="report">
+              <img src="https://yanxuan.nosdn.127.net/c365674c1a28e094e8b74339f3024749.jpeg?imageView&quality=75&thumbnail=550y356" alt="">
+              <div class="text">
+                <h3>网易严选3周年-粉丝生日趴</h3>
+                <p>3月末，我们分别在广州、北京、杭州的有家民宿严选HOME，还有长沙严选粉丝-来来的家 一起提前庆祝严选生日！</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
   
@@ -51,6 +229,9 @@
         } //可选选项，自动滑动
       }) 
       new BScroll('.Select',{})
+      new BScroll(this.$refs.pic,{
+        scrollX: true
+      })
     },
   }
 </script>
@@ -63,12 +244,233 @@
     padding 0
     padding-top 88px
     padding-bottom 150px
+    .monReport
+      font-size 24px
+      .pic
+        width 100%
+        height 377*2px
+        .reports
+          display flex
+          float left
+          .report
+            margin-right 32px
+            box-sizing border-box
+            padding 30px 17*2px
+            width 320*2px
+            height 377*2px
+            border 10px solid #e6e6e6
+            img 
+              width 276*2px
+              height 177*2px
+            .text
+              h3
+                font-size 34px
+                font-weight bold
+                margin 20px 0
+              p
+                font-size 28px
+                line-height 48px
+                color #7f7f7f
+      h2
+        width 100%
+        height 61*2px
+        font-size 30px
+        line-height 61*2px
+        color #000
+        text-align center
+    .result
+      padding 18*2px 15*2px 20*2px
+      color #7f7f7f
+      font-size 28px
+      .shops
+        display flex
+        flex-wrap wrap
+        .shop
+          box-sizing border-box
+          height  260*2px
+          width 330px
+          padding-bottom 46px
+          margin-right 15px
+          a
+            .text
+              padding-left 18px
+              .pir
+                color #b4282d;
+              .title
+                width 100%
+                color #333
+                overflow hidden
+                text-overflow ellipsis
+                white-space nowrap
+                margin-bottom 18px
+              .offer
+                width 64*2px
+                height 15*2px
+                line-height 28px
+                margin 20px 0
+                background-color #f48f18
+                color #fff
+                text-align center
+            .pic
+              position relative
+              width 168*2px
+              height 168*2px
+              background-color  #f4f4f4
+              img 
+                width 100%
+                height 100%
+              .chose
+                box-sizing border-box
+                position absolute
+                text-align center
+                left 0
+                top 0px
+                width 16*2px
+                height 62*2px
+                padding 16px 0  
+                font-size 22px
+                color #b4a078
+                border 2px solid #b4a078
+      .selHeader
+        padding 0px
+        color #7f7f7f
+    .tryOut
+      padding 18*2px 15*2px 20*2px
+      color #7f7f7f
+      font-size 28px
+      .content
+        text-align center
+        border 2px solid #ebebeb
+        .btn
+          margin 0 auto
+          margin-top 42px
+          margin-bottom 27*2px
+          box-sizing border-box
+          width 305*2px
+          height  88px
+          border 2px solid #dfdfdf
+          text-align center
+          line-height 88px
+        .card
+          height 84px
+          width 100%
+          display flex
+          justify-content space-between
+          align-items center
+          text-align center
+          div
+            width 50%
+            font-size 22px
+            .red
+              font-size 27px
+              color #AB2B2B
+              margin-bottom 10px
+            .black
+              font-size 27px
+              color #333
+              margin-bottom 10px
+            &:nth-child(1)
+              border-right 1px solid #d9d9d9
+            &:nth-child(2)
+              border-left 1px solid #d9d9d9
+             
+        .detail
+          h2
+            font-size 17*2px
+            color #333
+            margin 28px 0
+          .status
+            font-size 28px
+            margin-bottom 28px
+        .pic
+          position relative
+          img 
+            height 240*2px
+            width 100%
+            box-sizing border-box
+          .time
+            position absolute
+            left 50%
+            transform translateX(-50%)            
+            top 22px
+            width 320px
+            height 56*2px
+            background-color #7d7e7e
+            border-radius 50px
+            padding 0 17*2px
+            color #fff
+            text-align center
+            line-height 56px
+      .selHeader
+        padding 0px
+        color #7f7f7f
+    .selection
+      padding 18*2px 15*2px 20*2px
+      color #7f7f7f
+      font-size 28px
+      .selHeader
+        padding 0
+        margin-bottom 15px
+        border-bottom 1px solid #f2f2f2
+      .center
+        padding 18*2px 15*2px 20*2px
+        color #7f7f7f
+        font-size 28px
+        .shops
+          display flex
+          flex-wrap wrap
+          li
+            p
+              margin-top 12px
+              margin-bottom 26px 
+              span 
+                margin-right 12px
+                color #333
+            width 305px
+            height 252*2px
+            padding-right 10px
+            padding-bottom 48px
+            text-align center
+            .btn
+              margin 0 auto
+              height 22*2px
+              width 68*2px
+              color #EC9100
+              border 1px solid #EC9100
+              border-radius 22px
+              font-size 23px
+              text-align center
+              line-height 44px
+              i
+                font-size 28px
+            img 
+              width 100%
+              height 335px
+        .header
+          font-size 24px
+          div
+            height 38px
+            width 100%
+            padding-top 28px
+            padding-bottom 4px
+            .special
+              background-color #cfb495
+              color #fff
+              margin-right 10px
+            .title
+              color #333
     .collection
       padding 18*2px 15*2px 20*2px
       color #7f7f7f
       font-size 28px
       .btn
         width 100%
+        border 2px solid #bfbfbf
+        height 88px
+        text-align center
+        line-height 88px
+        margin-top 24px
+        color #333
       .text
         font-size 24px
         line-height 40px
